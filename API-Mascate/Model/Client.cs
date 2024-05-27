3b01d3ffd53d2b4697fd377b9b23e38c923ce821
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2024-05-27 10:32:41 -0300

    client Entity

--- API-Mascate/Model/Client.cs
+++ API-Mascate/Model/Client.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace API_Mascate.Model
 {
     [Table("client")]
-    public class Employee
+    public class Client
     {
 
         public int clientId { get; set; }
@@ -11,11 +11,14 @@ namespace API_Mascate.Model
         public int CPF { get; set; }
         public int AbsenceCount { get; set; }
 
-        public Employee(string name, int CPF, int AbsenceCount)
+        public Client(string name, int CPF, int AbsenceCount)
         {
             this.name = name ?? throw new ArgumentNullException(nameof(name));
             this.CPF = CPF;
             this.AbsenceCount = AbsenceCount;
+        }
+        public Client()
+        {
         }
     }
 }
